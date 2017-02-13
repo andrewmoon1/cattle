542d40b831818a680158d3e4c61fbd72dfa79826
andrewmoon1 <andrew.moon1@yahoo.com>
2017-02-12 20:44:50 -0600

    added foundation in a more appropriate manner

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import logo from '../../logo.svg';
+import './Header.scss';
 
 const Header = props => {
   return (
     <section className='header row'>
-      <button className='nav-btn small-1 columns'>|||</button>
+      <button className='nav-bttn show-for-small-only small-1  columns'>|||</button>
       <h2 className='headline small-8 columns'>Cattle</h2>
       <img src={logo} className="App-logo small-3 columns" alt="logo" />
     </section>
--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
-// require('bootstrap-loader');
 require('jquery');
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './style/index.css';
-import './style/foundation.min.css'
 
 ReactDOM.render(
   <App />,
